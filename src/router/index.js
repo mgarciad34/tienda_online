3228a8c12cae1a8b9c8c5b89a9b5c150a33483f2
Manuel Garcia <m.gd@outlook.es>
2024-11-08 07:29:44 +0100

    creamos funcionalidad registro

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import AdminDashboard from '../views/AdminDashboard.vue'
 import UsuarioDashboard from '../views/UsuarioDashboard.vue'
 import RegistroView from '../views/RegistroView.vue'
+import RecuperarView from '@/views/RecuperarView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +12,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/recuperar',
+      name: 'recuperar',
+      component: RecuperarView
     },
     {
       path: '/registro',
